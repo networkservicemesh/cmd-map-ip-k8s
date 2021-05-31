f6511668975c16c0320309dfe3ee0f6135bd2f77
Denis Tingaikin <denis.tingajkin@xored.com>
2021-06-01 01:13:27 +0700

    implement initial version of cmd-map-ip-k8s
    Signed-off-by: Denis Tingaikin <denis.tingajkin@xored.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,17 @@
-module github.com/networkservicemesh/cmd-template
+module github.com/networkservicemesh/cmd-map-ip-k8s
 
 go 1.16
+
+require (
+	github.com/antonfisher/nested-logrus-formatter v1.3.1
+	github.com/edwarnicke/serialize v1.0.7
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/networkservicemesh/sdk v0.0.0-20210531072719-eaf26022ad56
+	github.com/sirupsen/logrus v1.8.1
+	github.com/stretchr/testify v1.7.0
+	go.uber.org/goleak v1.1.10
+	gopkg.in/yaml.v2 v2.4.0
+	k8s.io/api v0.21.1
+	k8s.io/apimachinery v0.21.1
+	k8s.io/client-go v0.21.1
+)
--- main.go
+++ main.go
@@ -16,5 +16,108 @@
 
 package main
 
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	nested "github.com/antonfisher/nested-logrus-formatter"
+	"github.com/kelseyhightower/envconfig"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/networkservicemesh/cmd-map-ip-k8s/internal/mapipwriter"
+	"github.com/networkservicemesh/sdk/pkg/tools/log"
+	"github.com/networkservicemesh/sdk/pkg/tools/log/logruslogger"
+
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+)
+
+// Config represents the configuration for cmd-map-ip-k8s application
+type Config struct {
+	OutputPath string `default:"OutputPath" desc:"Path to writing map of internal to extenrnal ips"`
+}
+
 func main() {
+	// ********************************************************************************
+	// Configure signal handling context
+	// ********************************************************************************
+	ctx, cancel := signal.NotifyContext(
+		context.Background(),
+		os.Interrupt,
+		// More Linux signals here
+		syscall.SIGHUP,
+		syscall.SIGTERM,
+		syscall.SIGQUIT,
+	)
+	defer cancel()
+
+	// ********************************************************************************
+	// Setup logger
+	// ********************************************************************************
+	logrus.Info("Starting NetworkServiceMesh Client ...")
+	logrus.SetFormatter(&nested.Formatter{})
+	ctx = log.WithFields(ctx, map[string]interface{}{"cmd": os.Args[:1]})
+	ctx = log.WithLog(ctx, logruslogger.New(ctx))
+
+	logger := log.FromContext(ctx)
+
+	// ********************************************************************************
+	// Get config from environment
+	// ********************************************************************************
+	conf := &Config{}
+	if err := envconfig.Usage("nsm", conf); err != nil {
+		logger.Fatal(err)
+	}
+	if err := envconfig.Process("nsm", conf); err != nil {
+		logger.Fatalf("error processing rootConf from env: %+v", err)
+	}
+
+	// ********************************************************************************
+	// Create client-go
+	// ********************************************************************************
+	kubeConfig, err := rest.InClusterConfig()
+	if err != nil {
+		logger.Fatalf("can't get Kubernetes config. Are you running this app inside Kubernetes pod")
+	}
+	c, err := kubernetes.NewForConfig(kubeConfig)
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+
+	// ********************************************************************************
+	// Initialize goroutines for writing ips map
+	// ********************************************************************************
+	var mapWriter = mapipwriter.MapIPWriter{
+		OutputPath: conf.OutputPath,
+	}
+
+	list, err := c.CoreV1().Nodes().List(context.Background(), v1.ListOptions{})
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+
+	var eventsCh = make(chan watch.Event, len(list.Items))
+
+	watchClient, err := c.CoreV1().Nodes().Watch(ctx, v1.ListOptions{})
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+
+	go func() {
+		for i := 0; i < len(list.Items); i++ {
+			eventsCh <- watch.Event{Type: watch.Added, Object: &list.Items[i]}
+		}
+		for event := range watchClient.ResultChan() {
+			eventsCh <- event
+		}
+	}()
+
+	go mapWriter.Start(ctx, eventsCh)
+
+	<-ctx.Done()
 }
